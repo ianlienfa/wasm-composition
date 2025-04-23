970afcda4b70557bb8ce36f6ad2e56c88f4a4018
ianlin <ianlienfa@gmail.com>
2025-04-23 03:24:08 +0000

    WASI now has problem with inline WASM

--- src/host.cpp
+++ src/host.cpp
@@ -32,16 +32,18 @@ struct Wasm_Mod {
   std::vector<wasm_func_t*> func_exposed;
   std::string mod_name;
 
-  Wasm_Mod(std::string mod_name, wasm_engine_t* engine):
+  Wasm_Mod(std::string mod_name, wasm_engine_t* engine, bool std_inherit=true):
   mod_name(mod_name), engine(engine)
   {    
     wasm_extern_vec_new_empty(&imports);
     wasm_extern_vec_new_empty(&exports);
     store = wasm_store_new(engine);
     config = wasi_config_new(mod_name.c_str());
-    wasi_config_inherit_stdout(config);
-    wasi_config_inherit_stdin(config);
-    wasi_config_inherit_stderr(config);
+    if(std_inherit){
+      wasi_config_inherit_stdout(config);
+      wasi_config_inherit_stdin(config);
+      wasi_config_inherit_stderr(config);
+    }
   }  
 
   ~Wasm_Mod(){
@@ -89,15 +91,14 @@ struct Wasm_Mod {
       }
   } 
 
-  void wasmmod_load_module_from_bin(std::string filepath){
-    // Compile
-    wasm_byte_vec_t binary = load_wasm_binary(filepath);
+  // this deletes the binary vec 
+  void wasmmod_load_module_from_bytes(wasm_byte_vec_t *binary){
     printf("Compiling module...\n");
-    own module = wasm_module_new(store, &binary);
+    own module = wasm_module_new(store, binary);
     if (!module) {
       throw std::runtime_error("> Error compiling module!\n");
     }
-    wasm_byte_vec_delete(&binary);
+    wasm_byte_vec_delete(binary);
 
     // create wasi env
     wasi_env = wasi_env_new(store, config);
@@ -117,6 +118,20 @@ struct Wasm_Mod {
       print_wasmer_error();
       throw std::runtime_error("> Error getting WASI imports!\n");
       }
+  }
+
+  void wasmmod_load_module_from_str(const char* wat_string){
+    own wasm_byte_vec_t wat;
+    wasm_byte_vec_new(&wat, strlen(wat_string), wat_string);
+    own wasm_byte_vec_t wasm_bytes;
+    wat2wasm(&wat, &wasm_bytes);
+    wasm_byte_vec_delete(&wat); // own wasm_byte_vec_t wat
+    wasmmod_load_module_from_bytes(&wasm_bytes); // ownership trans
+  }
+
+  void wasmmod_load_module_from_file(std::string filepath){
+    wasm_byte_vec_t binary = load_wasm_binary(filepath);
+    wasmmod_load_module_from_bytes(&binary); // ownership trans
   }
   
   int wasmmod_run_start(){
@@ -193,6 +208,20 @@ void export_smth(){
 // }
 
 int main(int argc, const char* argv[]) {
+    const char* wat_string =
+        "(module\n"
+        "  (func $get_counter (import \"env\" \"get_counter\") (result i32))\n"
+        "  (func $add_to_counter (import \"env\" \"add_to_counter\") (param i32) (result i32))\n"
+        "  (type $increment_t (func (param i32) (result i32)))\n"
+        "  (func $increment_f (type $increment_t) (param $x i32) (result i32)\n"
+        "    (block\n"
+        "      (loop\n"
+        "        (call $add_to_counter (i32.const 1))\n"
+        "        (local.set $x (i32.sub (local.get $x) (i32.const 1)))\n"
+        "        (br_if 1 (i32.eq (local.get $x) (i32.const 0)))\n"
+        "        (br 0)))\n"
+        "    call $get_counter)\n"
+        "  (export \"increment_counter_loop\" (func $increment_f)))";
 
     // start wasm
     {
@@ -201,18 +230,14 @@ int main(int argc, const char* argv[]) {
       wasm_engine_t* engine = wasm_engine_new();
 
       // Initialize Wasm_Mod instances
-      Wasm_Mod a("a", engine);
+      Wasm_Mod a("a", engine, false /*std_inherit*/);
       printf("a initialized.\n");
-      Wasm_Mod b("b", engine);
+      Wasm_Mod b("b", engine, false /*std_inherit*/);
       printf("b initialized.\n");
             
-      a.wasmmod_load_module_from_bin("./a.wasm");
-      // no import, export handling, setup instance directly
-      // import function      
-
-      // import function to a done
+      a.wasmmod_load_module_from_str(wat_string);
       a.wasmmod_build_instance(); 
-      b.wasmmod_load_module_from_bin("./a.wasm");
+      b.wasmmod_load_module_from_str(wat_string);
       b.wasmmod_build_instance();
 
       a.wasmmod_run_start();
@@ -227,137 +252,3 @@ int main(int argc, const char* argv[]) {
 
   return 0;
 }
-
-
-// wasm_trap_t* host_func_callback(const wasm_val_vec_t* args, wasm_val_vec_t* results) {
-//   printf("Calling back...\n> ");
-
-//   wasm_val_t val = WASM_I32_VAL(42);
-//   wasm_val_copy(&results->data[0], &val);
-
-//   wasm_val_delete(&val);
-
-//   return NULL;
-// }
-
-// int main(int argc, const char* argv[]) {
-//   const char *wat_string =
-//       "(module\n"
-//       "  (func $host_function (import \"\" \"host_function\") (result i32))\n"
-//       "  (global $host_global (import \"env\" \"host_global\") i32)\n"
-//       "  (func $function (export \"guest_function\") (result i32) (global.get $global))\n"
-//       "  (global $global (export \"guest_global\") i32 (i32.const 42))\n"
-//       "  (table $table (export \"guest_table\") 1 1 funcref)\n"
-//       "  (memory $memory (export \"guest_memory\") 1))";
-
-//   wasm_byte_vec_t wat;
-//   wasm_byte_vec_new(&wat, strlen(wat_string), wat_string);
-//   wasm_byte_vec_t wasm_bytes;
-//   wat2wasm(&wat, &wasm_bytes);
-//   wasm_byte_vec_delete(&wat);
-
-//   printf("Creating the store...\n");
-//   wasm_engine_t* engine = wasm_engine_new();
-//   wasm_store_t* store = wasm_store_new(engine);
-
-//   printf("Compiling module...\n");
-//   wasm_module_t* module = wasm_module_new(store, &wasm_bytes);
-
-//   if (!module) {
-//       printf("> Error compiling module!\n");
-
-//       return 1;
-//   }
-
-//   wasm_byte_vec_delete(&wasm_bytes);
-
-//   printf("Creating the imported function...\n");
-//   wasm_functype_t* host_func_type = wasm_functype_new_0_1(wasm_valtype_new_i32());
-//   wasm_func_t* host_func = wasm_func_new(store, host_func_type, host_func_callback);
-//   wasm_functype_delete(host_func_type);
-
-//   printf("Creating the imported global...\n");
-//   wasm_globaltype_t* host_global_type = wasm_globaltype_new(wasm_valtype_new(WASM_F32), WASM_CONST);
-//   wasm_val_t host_global_val = WASM_I32_VAL(42);
-//   wasm_global_t* host_global = wasm_global_new(store, host_global_type, &host_global_val);
-//   wasm_globaltype_delete(host_global_type);
-
-//   wasm_extern_t* externs[] = {
-//       wasm_func_as_extern(host_func),
-//       wasm_global_as_extern(host_global)
-//   };
-
-//   wasm_extern_vec_t import_object = WASM_ARRAY_VEC(externs);
-
-//   printf("Instantiating module...\n");
-//   wasm_instance_t* instance = wasm_instance_new(store, module, &import_object, NULL);
-//   wasm_func_delete(host_func);
-//   wasm_global_delete(host_global);
-
-//   if (!instance) {
-//     printf("> Error instantiating module!\n");
-
-//     return 1;
-//   }
-
-//   printf("Retrieving exports...\n");
-//   wasm_extern_vec_t exports;
-//   wasm_instance_exports(instance, &exports);
-
-//   if (exports.size == 0) {
-//       printf("> Error accessing exports!\n");
-
-//       return 1;
-//   }
-
-//   printf("Retrieving the exported function...\n");
-//   wasm_func_t* func = wasm_extern_as_func(exports.data[0]);
-
-//   if (func == NULL) {
-//       printf("> Failed to get the exported function!\n");
-
-//       return 1;
-//   }
-
-//   printf("Got the exported function: %p\n", func);
-
-//   printf("Retrieving the exported global...\n");
-//   wasm_global_t* global = wasm_extern_as_global(exports.data[1]);
-
-//   if (global == NULL) {
-//       printf("> Failed to get the exported global!\n");
-
-//       return 1;
-//   }
-
-//   printf("Got the exported global: %p\n", global);
-
-//   printf("Retrieving the exported table...\n");
-//   wasm_table_t* table = wasm_extern_as_table(exports.data[2]);
-
-//   if (table == NULL) {
-//       printf("> Failed to get the exported table!\n");
-
-//       return 1;
-//   }
-
-//   printf("Got the exported table: %p\n", table);
-
-//   printf("Retrieving the exported memory...\n");
-//   wasm_memory_t* memory = wasm_extern_as_memory(exports.data[3]);
-
-//   if (memory == NULL) {
-//       printf("> Failed to get the exported memory!\n");
-
-//       return 1;
-//   }
-
-//   printf("Got the exported memory: %p\n", memory);
-
-//   wasm_module_delete(module);
-//   wasm_extern_vec_delete(&exports);
-//   wasm_instance_delete(instance);
-//   wasm_store_delete(store);
-//   wasm_engine_delete(engine);
-// }
-
