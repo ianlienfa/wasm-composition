1d790eec18b5e7798c258c1c9c8fd659103bf78f
ianlin <ianlienfa@gmail.com>
2025-04-23 03:08:42 +0000

    no mem leak now

--- src/a.cpp
+++ src/a.cpp
@@ -2,14 +2,34 @@
 #include <unistd.h>
 using namespace std;
 
-extern "C" __attribute__((used, visibility("default"))) volatile int glob_int = 42;
+#ifdef __cplusplus
+extern "C" {  // Prevent name mangling for C++ code
+#endif
 
-void print_glob(){
+__attribute__((used, visibility("default"))) volatile int glob_int = 42;
+void read_int();
+
+#ifdef __cplusplus
+}  // End extern "C" block
+#endif
+
+struct A {
+    int s;
+    int arr[100];
+};
+
+void read_int(){
     printf("glob_int: %d\n", glob_int);
+}  
+
+void read_a(A a){
+    a.arr[0] = 1;
 }
 
 int main(){
     cout << "wasm test!" << endl;
-    print_glob();
+    read_int();
+    A a;
+    read_a(a);
 }
 
